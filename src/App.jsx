d25cdb0793034e98994a09eb8f7f13827741852b
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-08-11 19:00:27 +0300

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import { lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import ModalsNav from './components/Modal/ModalsNav/ModalsNav';
+import { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import ModalsNav from "./components/Modal/ModalsNav/ModalsNav";
 
-
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
-const FavoritesPage = lazy(() => import('./pages/FavoritesPage/FavoritesPage'));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage/FavoritesPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
-
 
 function App() {
   return (
     <>
-    <Router basename="/Test-Task">
-      <Routes>
-        <Route path="/" element={<Layout />} >
-        <Route index element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<NotFoundPage to="/" replace />} />
-        </Route>
-      </Routes>
-    </Router>
-    <ModalsNav />
+      <Router basename="/Test-Task">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="catalog" element={<CatalogPage />} />
+            <Route path="favorites" element={<FavoritesPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+
+      <ModalsNav />
     </>
   );
 }
