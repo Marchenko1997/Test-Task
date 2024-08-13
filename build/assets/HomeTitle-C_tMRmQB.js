36bc42603f7899211f38ab7806f8e438ba8e0f3a
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-08-13 18:02:59 +0300

    error pages

--- build/assets/HomeTitle-C_tMRmQB.js
+++
@@ -1,1 +0,0 @@
-import{P as s,j as e,H as t}from"./index-CAdOF_Ob.js";function i({children:r}){return e.jsx(t,{children:e.jsx("title",{children:r})})}i.propTypes={children:s.node.isRequired};export{i as H};
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { lazy } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import ModalsNav from "./components/Modal/ModalsNav/ModalsNav";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
 const FavoritesPage = lazy(() => import("./pages/FavoritesPage/FavoritesPage"));
-// const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 
 function App() {
